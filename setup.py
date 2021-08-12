00dfb361ce3196c625e3c819bbfe2a3d920c4933
torgbuiedunyenyo <jjkirshbaum@gmail.com>
2021-08-12 10:50:29 -0700

    update

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='pet',
+    version='',
+    packages=['pet', 'examples'],
+    url='',
+    license='',
+    author='ratpartyserver',
+    author_email='',
+    description=''
+)
